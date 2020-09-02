5a55c292468fd700b2e1159e433adcb3c7114f4e
Thiago Felipe <tf.arcanjo@gmail.com>
2020-09-02 20:47:42 -0300

    Criando Rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import DashBoard from './pages';
+import Repository from './pages/Repository'
+
+const Routes: React.FC = () => {
+  return (
+    < Switch >
+      <Route path="/" exact component={DashBoard} />
+    </Switch >
+  )
+}
+
+export default Routes;
