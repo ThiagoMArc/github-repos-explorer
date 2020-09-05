5fcf049ed66668fbf2617814a63e6651af5cc7bb
Thiago Felipe <tf.arcanjo@gmail.com>
2020-09-05 12:40:59 -0300

    Estilizando a dashboard

--- src/routes/pages/Dashboard/index.tsx
+++ src/routes/pages/Dashboard/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Title } from './style'
+import { Title, Form, Repositories } from './style'
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../../assets/logo-expl.svg'
+
 
 const DashBoard: React.FC = () => {
   return (
     <>
+      <img src={logoImg} alt='Github Explorer' />
       <Title>Explore Repositórios no github</Title>
-
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="button">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img src="" alt="" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p></p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   )
 }
